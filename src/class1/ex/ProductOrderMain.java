9ff8bd15c792b6163a3f19468303c17081d081b5
xyzli <kyu_107@naver.com>
2025-08-11 15:42:24 +0900

    class1

---
+++ src/class1/ex/ProductOrderMain.java
@@ -0,0 +1,37 @@
+package class1.ex;
+
+public class ProductOrderMain {
+    public static void main(String[] args) {
+
+        ProductOrder[] productOrders = new ProductOrder[3];
+
+        ProductOrder po1 = new ProductOrder();
+        po1.productName = "두부";
+        po1.price = 2000;
+        po1.quantity = 2;
+
+        ProductOrder po2 = new ProductOrder();
+        po2.productName = "김치";
+        po2.price = 5000;
+        po2.quantity = 1;
+
+        ProductOrder po3 = new ProductOrder();
+        po3.productName = "콜라";
+        po3.price = 1500;
+        po3.quantity = 2;
+
+        productOrders[0] = po1;
+        productOrders[1] = po2;
+        productOrders[2] = po3;
+
+        int totalAmount = 0;
+
+        for (ProductOrder productOrder : productOrders) {
+            System.out.println("상품명: " + productOrder.productName + ", 가격: " + productOrder.price + ", 수량: " + productOrder.quantity);
+            totalAmount += productOrder.quantity * productOrder.price;
+        }
+
+        /*int total = (po1.price * po1.quantity) + (po2.price * po2.quantity) + (po3.price * po3.quantity);*/
+        System.out.println("총 결제 금액: " + totalAmount);
+    }
+}
