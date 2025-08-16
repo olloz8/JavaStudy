212a32fd404d90f0fe1570f4bcb50890591bf172
xyzli <kyu_107@naver.com>
2025-08-16 16:42:31 +0900

    pack

---
+++ src/pack/a/User2.java
@@ -0,0 +1,8 @@
+package pack.a;
+
+public class User2 {
+
+    public User2() {
+        System.out.println("피키지 pack.a 회원2 생성");
+    }
+}
---
+++ src/pack/b/User.java
@@ -0,0 +1,8 @@
+package pack.b;
+
+public class User {
+
+    public User() {
+        System.out.println("피키지 pack.b 회원 생성");
+    }
+}
