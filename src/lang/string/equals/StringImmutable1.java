aa1f4a2b9af907db357ba030d17eee7835a3746e
xyzli <kyu_107@naver.com>
2025-09-19 18:45:45 +0900

    equals

---
+++ src/lang/string/equals/StringImmutable1.java
@@ -0,0 +1,10 @@
+package lang.string.equals;
+
+public class StringImmutable1 {
+
+    public static void main(String[] args) {
+        String str = "hello";
+        str.concat(" java");
+        System.out.println("str = " + str);
+    }
+}
