b11c27e79acbc7a5ed65671540d32b7594c3ecf2
xyzli <kyu_107@naver.com>
2025-09-20 18:18:56 +0900

    method

---
+++ src/lang/string/method/StringChngeMain2.java
@@ -0,0 +1,15 @@
+package lang.string.method;
+
+public class StringChngeMain2 {
+    public static void main(String[] args) {
+        String strWithSpaces = "      Java Programing";
+
+        System.out.println("소문자로 변환 : " + strWithSpaces.toLowerCase());
+        System.out.println("대문자로 변환 : " + strWithSpaces.toUpperCase());
+
+        System.out.println("공백 제거(trim) : '" + strWithSpaces.trim() + "'");
+        System.out.println("공백 제거(strip) : '" + strWithSpaces.strip() + "'");
+        System.out.println("앞 공백 제거(strip) : '" + strWithSpaces.stripLeading() + "'");
+        System.out.println("뒤 공백 제거(strip) : '" + strWithSpaces.stripTrailing() + "'");
+    }
+}
